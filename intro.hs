fb74402a1b3edc0c42dea5877a7faa941c64b12e
Karun Ramakrishnan <kramakrishnan@cj.com>
2014-05-13 19:17:24 -0700

    Solutions for sumDigits, and validate

--- intro.hs
+++ intro.hs
@@ -11,6 +11,8 @@ modString = mod . charToInt
 reverse' :: [Integer] -> [Integer]
 reverse' xs = error "todo"
 
+isValid :: Integer -> Bool
+isValid n = n == 0
 -- | Convert a number to a list of digits
 --
 -- Examples:
@@ -51,3 +53,30 @@ toDigitsRev = reverse . toDigits
 --
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther = zipWith (*) (cycle [1,2])
+
+
+-- | Sum all the digits in a list
+--
+-- Examples:
+--
+-- >>> sumDigits [1,2,3]
+-- 6
+--
+-- >>> sumDigits [1, 12, 123]
+-- 10
+--
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . concat . map toDigits
+
+-- | Validates a credit card number based on the conditions set in the assignment
+--
+-- Examples:
+--
+-- >>> validate 4012888888881881
+-- False
+--
+-- >>> validate 4012888888881882
+-- True
+--
+validate :: Integer -> Bool
+validate = isValid . flip rem 10 . sumDigits . toDigitsRev
