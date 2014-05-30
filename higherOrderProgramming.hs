3f9e4b50d06e589c5b42aa8eefea87caedf54295
Karun <karun012@gmail.com>
2014-05-29 23:05:09 -0700

    Solution for fun1'

--- higherOrderProgramming.hs
+++ higherOrderProgramming.hs
@@ -1,13 +1,28 @@
 module HigherOrderProgramming where
 
+-- | Runs (x - 2) on all even numbers
+--
+-- >>> fun1 [10,12,14,16]
+-- 13440
+--
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
     | even x = (x - 2) * fun1 xs
     | otherwise = fun1 xs
 
+-- | A more wholemeal implementation of fun1
+--
+-- >>> fun1' [10,12,14,16]
+-- 13440
+--
+fun1' :: [Integer] -> Integer
+fun1' = foldr((*) . (-) 2) 1 . filter even
+
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n
     | even n = n + fun2 (n `div` 2)
     | otherwise = fun2 (3 * n + 1)
+    
+
