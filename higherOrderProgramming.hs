9ca2a916807a0ff0f3b0a588dbf18b1901c70070
Karun Ramakrishnan <kramakrishnan@cj.com>
2014-06-02 16:13:14 -0700

    Solution for fun2'

--- higherOrderProgramming.hs
+++ higherOrderProgramming.hs
@@ -1,6 +1,6 @@
 module HigherOrderProgramming where
 
--- | Runs (x - 2) on all even numbers
+-- | Runs (x - 2) on all even numbers, and sums them up
 --
 -- >>> fun1 [10,12,14,16]
 -- 13440
@@ -19,10 +19,25 @@ fun1 (x:xs)
 fun1' :: [Integer] -> Integer
 fun1' = product . map ((-) 2) . filter even
 
+-- | Some crazy integer manipulation
+--
+-- >>> fun2 9
+-- 276
+--
+-- >>> fun2 12
+-- 58
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n
     | even n = n + fun2 (n `div` 2)
     | otherwise = fun2 (3 * n + 1)
-    
 
+-- | A more wholemeal implementation of fun2
+--
+-- >>> fun2' 9
+-- 276
+--
+-- >>> fun2' 12
+-- 58
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (> 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
