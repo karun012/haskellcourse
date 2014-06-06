5761f54672eedf6e6034654b582beed88e7ddd8d
Karun Ramakrishnan <kramakrishnan@cj.com>
2014-06-06 09:06:47 -0700

    Instance Expr for Integers

--- polymorphismAndTypeClasses.hs
+++ polymorphismAndTypeClasses.hs
@@ -19,10 +19,10 @@ eval expression = case expression of
                   Mul x y -> eval x * eval y
 
 -- | Evaluates arithmetic expressions given as strings
--- 
+--
 -- >>> evalStr "(2+3)*3"
 -- Just 15
--- 
+--
 -- >>> evalStr "(3+3) + (2+2) * 10"
 -- Just 46
 --
@@ -36,13 +36,23 @@ evalStr str = eval <$> parseExp Lit Add Mul str
 --
 -- >>> mul (add (lit 2) (lit 3)) (lit 4) :: ExprT
 -- Mul (Add (Lit 2) (Lit 3)) (Lit 4)
--- 
+--
 class Expr a where
   add :: a -> a -> a
   mul :: a -> a -> a
   lit :: Integer -> a
 
 instance Expr ExprT where
-  lit = Lit 
-  add = Add 
-  mul = Mul 
+  lit = Lit
+  add = Add
+  mul = Mul
+
+
+-- | Instance of Expr for Integer
+--
+-- >>> (mul (add (lit 2) (lit 3)) (lit 3)) :: Integer
+-- 15
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
