4ba2c622b68cc89f3f4b4bbd25e2dfdfc370f3c4
Karun Ramakrishnan <kramakrishnan@cj.com>
2014-06-30 09:02:22 -0700

    Solution for fibs1

--- src/lazyEvaluation.hs
+++ src/lazyEvaluation.hs
@@ -18,3 +18,14 @@ fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
 fib n = fib (n - 2) + fib (n - 1)
+
+-- | Generates an infinite list of fibonacci numbers
+--
+-- >>> take 1 $ fibs1
+-- [0]
+--
+-- >>> take 5 $ fibs1
+-- [0,1,1,2,3]
+--
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
