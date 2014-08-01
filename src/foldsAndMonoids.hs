9189f73ba10021b346f026457e36b8dcaf3a3464
karun012 <karun012@gmail.com>
2014-07-31 17:33:13 -0700

    Implemented tag

--- src/foldsAndMonoids.hs
+++ src/foldsAndMonoids.hs
@@ -1,2 +1,16 @@
-moudle FoldsAndMonoids where
+module FoldsAndMonoids where
 
+import JoinList
+import Data.Monoid
+
+-- | Gets the annotation at the root of a JoinList
+--
+-- >>> tag (Single (Product 5) 'y')
+-- Product {getProduct = 5}
+--
+-- >>> tag (Append (Product 6) (Single (Product 2) 'e') (Single (Product 3) 'a'))
+-- Product {getProduct = 6}
+--
+tag :: Monoid m => JoinList m a -> m
+tag (Single m _) = m
+tag (Append m _ _) = m
